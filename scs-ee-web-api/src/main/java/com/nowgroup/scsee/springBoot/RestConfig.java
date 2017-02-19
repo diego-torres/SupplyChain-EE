3720cb57dba308ce09a57ab50e75f507d142e51b
diego-torres <dtorresf@redhat.com>
2017-02-19 12:07:02 -0600

    Part REST controller

--- scs-ee-web-api/src/main/java/com/nowgroup/scsee/springBoot/RestConfig.java
+++ scs-ee-web-api/src/main/java/com/nowgroup/scsee/springBoot/RestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.web.filter.CorsFilter;
 
 import com.nowgroup.scsee.authentication.AuthenticationRestController;
 import com.nowgroup.scsee.cat.company.CompanyRestController;
+import com.nowgroup.scsee.cat.part.PartRestController;
 import com.nowgroup.scsee.cat.storage.StorageRestController;
 import com.nowgroup.scsee.cat.unit.UnitRestController;
 import com.nowgroup.scsee.geo.country.CountryRestController;
@@ -19,7 +20,7 @@ import com.nowgroup.scsee.packingList.PackingListRestController;
 @Configuration
 @ComponentScan(basePackageClasses = { CompanyRestController.class, StorageRestController.class,
 		UnitRestController.class, PackingListRestController.class, AuthenticationRestController.class,
-		CountryRestController.class, GeoStateRestController.class })
+		CountryRestController.class, GeoStateRestController.class, PartRestController.class })
 public class RestConfig {
 	@Bean
 	public FilterRegistrationBean corsFilter() {
